67822a06d40917276fc42663775d240c922e5c4a
Joel <joelmarfas@gmail.com>
2021-10-18 18:11:33 +0200

    link added + styles and heading under development

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import TaskList from "./components/TaskList"
 function App() {
   return (
     <div>
+      <div className="h-screen text-white text-center p-10">
+        <div className="container mx-auto h-full">
       <Heading />
       <Switch>
         <Route path="/" component={TaskList} exact />
         <Route path="/add" component={TaskForm}/>
       </Switch>
+        </div>
+      </div>
     </div>
   );
 }
