beac4474311ff7968293e86bb9c5f65ffa9b9a77
Joel <joelmarfas@gmail.com>
2021-10-19 12:25:29 +0200

    global context created

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import { FaPlus } from "react-icons/fa"
 
 const Heading = () => {
   return (
@@ -11,7 +12,7 @@ const Heading = () => {
         <div className="flex-grow text-right px-4 py-2 m-2">
           <Link to="/add">
             <button className="bg-green-400 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
-              Add Employee
+            <FaPlus /> Add Employee 
             </button>
           </Link>
         </div>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalContext} from "../context/GlobalContext"
 
 const TaskList = () => {
+
+  const context = useContext(GlobalContext)
+  console.log(context)
+
   return (
     <div>
       TaskList
