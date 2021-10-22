84f08ef063efac33e57245b35a4878e5fa18f411
Joel <joelmarfas@gmail.com>
2021-10-22 12:35:58 +0200

    login skeleton done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Formik } from 'formik';
 
 function Login() {
+
+
   return (
-    <div>
-      Login Page
-    </div>
+
+    <Formik 
+      
+    >
+      {() => ( //estructura renderer prop
+        <form>
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" name="name" placeholder="Joel Martinez"/>
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email" name="email" placeholder="example@example.com"/>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      )}
+    </Formik>
   )
 }
 
