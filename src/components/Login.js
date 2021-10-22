90218ab52f126940e7c0628463fe19327a8b6894
Joel <joelmarfas@gmail.com>
2021-10-22 16:27:42 +0200

    formik functions validation in prosess

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,17 +7,30 @@ function Login() {
   return (
 
     <Formik 
-      
+      initialValues={{
+        name: "",
+        email: ""
+
+      }}
+      validate={(values) => {
+        if(!values.name) {
+          console.log("")
+        }
+      }}
+      onSubmit={(values) =>{
+        console.log(values);
+        console.log("formulario enviado");
+      }}
     >
-      {() => ( //estructura renderer prop
-        <form>
+      {({values, handleSubmit, handleChange, handleBlur}) => ( //estructura renderer prop
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Name: </label>
-            <input type="text" id="name" name="name" placeholder="Joel Martinez"/>
+            <input type="text" id="name" name="name" placeholder="Joel Martinez" value={values.name} onChange={handleChange} onBlur={handleBlur}/>
           </div>
           <div>
             <label htmlFor="email">Email: </label>
-            <input type="email" id="email" name="email" placeholder="example@example.com"/>
+            <input type="email" id="email" name="email" placeholder="example@example.com" value={values.email} onChange={handleChange} onBlur={handleBlur}/>
           </div>
           <button type="submit">Login</button>
         </form>
